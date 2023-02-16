c83dc0bfbec4f8f38921651cd1cde59118dc2b59
valeroussecurity <87388093+valeroussecurity@users.noreply.github.com>
2023-02-16 09:41:40 -0700

    Move_Files Added

---
+++ Move.ps1
@@ -0,0 +1,7 @@
+﻿# Replace "<SERVER_NAME_HERE!>" with the name of your server. Source server is where you want the data to copy from, Destination is where it will go to.
+# If files are already in the destination directory, it will overwrite them.
+
+$sourcePath = "\\<SERVER_NAME_HERE!>\D$\Shares"
+$destinationPath = "\\<SERVER_NAME_HERE!>\D$\Shares"
+
+Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse -Force
